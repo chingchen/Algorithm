0597c2df4b01181082d835e969c851256deadf56
ching <chingc@outlook.com>
2012-12-01 11:01:11 +0800

    update

--- Word Search.cpp
+++ Word Search.cpp
@@ -1,7 +1,8 @@
 /*
 Given a 2D board and a word, find if the word exists in the grid.
 
-The word can be constructed from letters of sequentially adjacent cell, where “adjacent” cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
+The word can be constructed from letters of sequentially adjacent cell, where adjacent” 
+cells are those horizontally or vertically neighboring. The same letter cell may not be used more than once.
 
 For example,
 Given board =
@@ -11,9 +12,9 @@ Given board =
 ["SFCS"],
 ["ADEE"]
 ]
-word = “ABCCED”, -> returns true,
-word = “SEE”, -> returns true,
-word = “ABCB”, -> returns false
+word = ABCCED”, -> returns true,
+word = CEE”, -> returns true,
+word = ABCB”, -> returns false
 */
 
 class Solution 
