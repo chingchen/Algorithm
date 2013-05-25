567685ac5a68c8c5f274954cf06e0b3c6540c8b1
ching <chingc@outlook.com>
2013-05-25 10:24:30 +0800

    update

--- Binary Tree Maximum Path Sum.cpp
+++ Binary Tree Maximum Path Sum.cpp
@@ -1,44 +1,35 @@
 class Solution {
 public:
-	int maxPathSum(TreeNode *root) {
-		// Start typing your C/C++ solution below
-		// DO NOT write int main() function
-		if(root == NULL)
-			return 0;
-		max = root->val;
-		GetMax(root);
-		return max;
-	}
-private:
-	int max  ;
-	int GetMax( TreeNode * root)
+    int maxPathSum(TreeNode *root) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+        int max = root->val;
+	maxSum(root,max);
+	return max;
+    }
+    int maxSum(TreeNode * root,int & max)
+   {
+	int left = 0 ,right =0 ;
+	if( root->left != NULL )
+		left = maxSum(root->left,max);
+	if( root->right != NULL )
+		right = maxSum(root->right,max);
+
+	int addition = root->val ;
+	if( left > 0)  addition += left;
+	if( right > 0) addition += right;
+	if( addition  > max )
+		max = addition ;
+	if( left > right )
 	{
-		if(root == NULL)
-			return 0;
-		int leftMax = GetMax(root->left);
-		int rightMax = GetMax(root->right);
-
-		int currentMax = root->val;
-		if(leftMax >0)
-			currentMax += leftMax;
-		if(rightMax > 0)
-			currentMax += rightMax;
-		if(currentMax > max)
-			max = currentMax;
-
-		if(leftMax > rightMax)	
-		{
-			if(leftMax >0)
-				return root->val + leftMax;
-			else 
-				return root->val;
-		}
-		else		
-		{
-			if(rightMax >0)
-				return root->val + rightMax;
-			else
-				return root->val;	
-		}
+		if(left > 0)
+			return left + root->val;
 	}
+	else
+	{
+		if(right > 0)
+			return right + root->val;
+	}
+	return root->val;
+    }
 };
