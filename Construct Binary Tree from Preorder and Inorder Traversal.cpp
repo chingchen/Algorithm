a5cd39dd23e7684101e6c2525680fbf66fe5eeae
ching <chingc@outlook.com>
2013-08-20 08:27:38 +0800

    add two

--- Construct Binary Tree from Preorder and Inorder Traversal.cpp
+++ Construct Binary Tree from Preorder and Inorder Traversal.cpp
@@ -5,65 +5,58 @@ Note:
 You may assume that duplicates do not exist in the tree.
 */
 
-#include <iostream>
-#include <vector>
-#include <list>
-#include <algorithm>
-using namespace std;
-
-struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
-};
-
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
 class Solution {
+    
+map<int, int> hashMap;
 public:
-	TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
-		// Start typing your C/C++ solution below
-		// DO NOT write int main() function
-		if(inorder.size() == 0)
-			return NULL;
-		Clean();
-		Build(inorder,preorder,0,preorder.size()-1,0);
-		return allNodes[0];
-	}
-
-private:
-	TreeNode * Build(vector<int> &inorder, vector<int> &preorder,int start ,int end,int inorderPos)
-	{
-		TreeNode *node = new TreeNode(preorder[inorderPos]);
-		allNodes.push_back(node);
-		int pos = find(inorder.begin()+start, inorder.begin()+end,preorder[inorderPos]) - inorder.begin();
-		if(pos == end)
-			node->right = NULL;
-		else
-			node ->right = Build(inorder, preorder,pos+1,end,inorderPos + pos - start +1);
-		if(pos == start)
-			node-> left = NULL;
-		else
-			node ->left = Build(inorder,preorder,start,pos-1,inorderPos+1);
-		return node;
-	}
-
-	void Clean()
-	{
-		int len = allNodes.size();
-		for(int i = 0; i < len ;i++)
-			delete allNodes[i];
-		allNodes.clear();
-	}
-	vector<TreeNode*> allNodes;
+    TreeNode *buildTree(vector<int> &preorder, vector<int> &inorder) {
+        // Start typing your C/C++ solution below
+        // DO NOT write int main() function
+         if( preorder.size() == 0)
+            return NULL;
+         hashMap.clear();
+         for( int i = 0 ; i < inorder.size(); i++)
+         {
+             hashMap[inorder[i]] = i;
+         }
+         
+         return buildTree(preorder, 0, inorder, 0, inorder.size()-1);
+    }
+    
+    TreeNode * buildTree(vector<int>& preorder, int preIndex, vector<int> & inorder, int inBegin, int inEnd)
+    {
+        TreeNode * head = new TreeNode( preorder[preIndex]);
+        
+        int inIndex = hashMap[ preorder[preIndex]];       
+        
+        
+        if( inIndex != inBegin )
+        {
+            head -> left = buildTree( preorder ,preIndex+1, inorder, inBegin, inIndex-1);
+        }
+        else
+        {
+            head -> left = NULL;            
+        }
+        
+        if(inIndex != inEnd)
+        {
+            head->right = buildTree( preorder, preIndex + inIndex - inBegin + 1, inorder, inIndex+1, inEnd);
+        }
+        else
+        {
+            head->right = NULL;
+        }
+        
+        return head;
+    }
 };
-
-int main()
-{
-	Solution s;
-	int in[]={1,2,3,4};
-	int post[]={2,3,1,4};
-
-	vector<int> inVec(in,in+sizeof(in)/sizeof(in[0]));
-	vector<int > outvec(post,post+sizeof(post)/sizeof(post[0]));
-	TreeNode* tree = s.buildTree(inVec,outvec);
-}
